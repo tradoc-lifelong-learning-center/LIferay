8e7ce0c5b76b8a382645cceb480c4ba8ab6e37cd
Mike Dean <mldean@users.noreply.github.com>
2019-04-01 11:04:08 -0400

    cleanup

--- portlets/publication-selector-portlet/src/main/java/com/tjaglcs/plugins/Article.java
+++ portlets/publication-selector-portlet/src/main/java/com/tjaglcs/plugins/Article.java
@@ -3,53 +3,18 @@ package com.tjaglcs.plugins;
 public class Article {
 	private long articleId;
 	private int volume;
-	//private int issue;
-	
-	
 	
 	public Article(long articleId, int volume) {
 		this.articleId = articleId;
 		this.volume = volume;
-		//this.issue = issue;
 	}
-	
-	
-	public String getQueryString() {
-		
-		String queryString = "";
-		queryString+="?articleId=";
-		queryString+=articleId;
-		
-		queryString+="&vol=";
-		queryString+=volume;
-		
-		//queryString+="&no=";
-		//queryString+=issue;
-		
-		return queryString;
-	}
-
 
 	public long getArticleId() {
 		return articleId;
 	}
 
-
-
-
-
 	public int getVolume() {
 		return volume;
 	}
-
-
-
-
-
-	//public int getIssue() {
-	//	return issue;
-	//}
-
-
 
 }
