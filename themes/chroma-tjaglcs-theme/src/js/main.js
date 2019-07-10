6f3aae9c13b804f5a37a908bed96958870c53d84
Mike Dean <mldean@users.noreply.github.com>
2019-07-10 11:09:45 -0400

    Clearing old JS

--- themes/chroma-tjaglcs-theme/src/js/main.js
+++ themes/chroma-tjaglcs-theme/src/js/main.js
@@ -12,99 +12,18 @@
  * details.
  */
 AUI().ready(
-	'aui-carousel', 'liferay-hudcrumbs', 'liferay-navigation-interaction', 'event-outside', 'aui-sortable-layout', 'aui-toggler',
 	function(A) {
-		var navigation = A.one('#navigation');
+		var showSearchButton = A.one('#show-search-button');
+		var searchBarContainer = A.one('#search-bar-container');
 
-		if (navigation) {
-			navigation.plug(Liferay.NavigationInteraction);
-		}
+		showSearchButton.onclick = function(){
+			console.log("click!");
+		};
 
-		var siteBreadcrumbs = A.one('#breadcrumbs');
+		console.log("hello world!")
+		console.log("A: ")
+		console.log(A)
 
-		if (siteBreadcrumbs) {
-			siteBreadcrumbs.plug(A.Hudcrumbs);
-		}
 
-		var dockbar = A.one('.color-scheme-default .dockbar');
-
-		if (dockbar) {
-			A.all('#navigation .nav-item-dockbartoggle > a').each(function(btnDockbarToggle) {
-				new A.Toggler(
-					{
-						content: dockbar,
-						header: btnDockbarToggle,
-						expanded: false
-					}
-				);
-			});
-		}
-
-		var banner = A.one('.color-scheme-dashboard #banner');
-		var btnNavigation = A.one('#navigation .nav-item-sitenavigationtoggle > a');
-
-		if (banner) {
-			var btnNavigationDockbar = A.one('#_145_navSiteNavigationNavbarBtn');
-
-			if (!btnNavigationDockbar) {
-				btnNavigationDockbar = btnNavigation;
-			}
-			else {
-				btnNavigation.hide();
-			}
-
-			new A.Toggler(
-				{
-					content: banner,
-					header: btnNavigationDockbar,
-					expanded: false
-				}
-			);
-		}
-		else {
-			var navigation = A.one(Liferay.Data.NAV_SELECTOR);
-
-			if (btnNavigation) {
-				btnNavigation.on(
-					'click',
-					function(event) {
-						btnNavigation.toggleClass('open');
-						navigation.toggleClass('open');
-					}
-				);
-			}
-		}
-
-		var siteSearch = A.one('#sitesearch');
-
-		if (siteSearch) {
-			var btnSearch = siteSearch.one('a');
-
-			var searchForm = siteSearch.one('.site-search-form');
-			var searchField = searchForm.one('.site-search-field');
-
-			btnSearch.on(
-				'click',
-				function(event) {
-					event.preventDefault();
-
-					if (searchForm.getStyle('width') == '0px') {
-						siteSearch.removeClass('site-search-collapsed');
-						searchField.focus();
-
-						return;
-					}
-					else {
-						if (searchField.get('value') == '') {
-							siteSearch.addClass('site-search-collapsed');
-
-							return;
-						}
-					}
-
-					searchForm.submit();
-				}
-			);
-		}
 	}
 );
