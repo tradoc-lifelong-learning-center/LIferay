7fb7ca6f3282f0b8e8a522568b62b70d777e9e4b
Mike Dean <mldean@users.noreply.github.com>
2019-07-11 09:06:50 -0400

    Cleanup

--- themes/chroma-tjaglcs-theme/src/js/main.js
+++ themes/chroma-tjaglcs-theme/src/js/main.js
@@ -1,74 +1,32 @@
-/*
- * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
- *
- * This library is free software; you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation; either version 2.1 of the License, or (at your option)
- * any later version.
- *
- * This library is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
- */
+
 AUI().ready(
 	function(A) {
-		/*//instead of making button and wrapper, can I intercept existing button IF the search is not expanded?
-	  var showSearchButton = A.one('#showSearchButton');
-		//var searchBarContainer = A.one('#search-bar-container');
-		//var showSearchButton = document.getElementById("showSearchButton");
-	  showSearchButton._node.onclick = function(){
-			console.log("click!");
-		};
 
-		console.log("showSearchButton: ")
-		console.log(showSearchButton)
-		console.log(showSearchButton._node)
 
-		console.log("A: ")
-		console.log(A)
-*/
-
+		//Show site search bar on click
 		var siteSearch = A.one('#siteSearch');
-		console.log("siteSearch")
-		console.log(siteSearch)
 
 		if (siteSearch) {
 			var btnSearch = siteSearch.one('button');
 
 			var searchForm = siteSearch.one('form');
-			//var searchField = searchForm.one('.site-search-field');
-			//TODO: figure out a better way to get this. Instance id will change.
 			var searchField = searchForm.one('.search-bar-keywords-input');
-
-
 
 			btnSearch.on(
 				'click',
 				function(event) {
 					event.preventDefault();
 
-					//alert("click!")
-					console.log("searchForm: ");
-					console.log(searchForm);
-					console.log("width: " + searchForm.getStyle('width'));
-					console.log("searchField: ");
-					console.log(searchField);
-					console.log("width: " + searchField.getStyle('width'));
-
 					if (searchField.getStyle('width') == '0px') {
-						//siteSearch.removeClass('site-search-collapsed');
 						searchField.setStyle('width', '160px');
 						searchField.setStyle('padding-left', '.5rem');
 						searchField.setStyle('padding-right', '.5rem');
 						searchField.focus();
-						//console.log("hey!")
 
 						return;
 					}
 					else {
 						if (searchField.get('value') == '') {
-							//siteSearch.addClass('site-search-collapsed');
 							searchField.setStyle('width', '0px');
 							searchField.setStyle('padding-left', '0');
 							searchField.setStyle('padding-right', '0');
