a03648045172b6e59f62303c0ccfa2f40d59df41
Mike Dean <mldean@users.noreply.github.com>
2019-07-25 13:24:48 -0400

    Updating search bar to use classes

--- themes/chroma-tjaglcs-theme/src/js/main.js
+++ themes/chroma-tjaglcs-theme/src/js/main.js
@@ -1,46 +1,43 @@
-
 AUI().ready(
-	function(A) {
+  function(A) {
 
 
-		//Show site search bar on click
-		var siteSearch = A.one('#siteSearch');
+    //Show site search bar on click
+    var siteSearch = A.one('#siteSearch');
 
-		if (siteSearch) {
-			var btnSearch = siteSearch.one('button');
+    if (siteSearch) {
+      var btnSearch = siteSearch.one('button');
 
-			var searchForm = siteSearch.one('form');
-			var searchField = searchForm.one('.search-bar-keywords-input');
-
-			btnSearch.on(
-				'click',
-				function(event) {
-					event.preventDefault();
-
-					if (searchField.getStyle('width') == '0px') {
-						searchField.setStyle('width', '160px');
-						searchField.setStyle('padding-left', '.5rem');
-						searchField.setStyle('padding-right', '.5rem');
-						searchField.focus();
-
-						return;
-					}
-					else {
-						if (searchField.get('value') == '') {
-							searchField.setStyle('width', '0px');
-							searchField.setStyle('padding-left', '0');
-							searchField.setStyle('padding-right', '0');
+      var searchForm = siteSearch.one('form');
+      var searchField = searchForm.one('.search-bar-keywords-input');
 
 
-							return;
-						}
-					}
 
-					searchField.set('value','');
-					searchForm.submit();
-				}
-			);
-		}
+      btnSearch.on(
+        'click',
+        function(event) {
+          event.preventDefault();
 
-	}
+
+
+          if (searchField.getStyle('width') == '0px') {
+            searchField._node.classList.add('search-bar-keywords-input--expanded');
+						btnSearch._node.classList.add('search-bar-search-button--selected');
+            searchField.focus();
+            return;
+          } else {
+            if (searchField.get('value') == '') {
+              searchField._node.classList.remove('search-bar-keywords-input--expanded');
+							btnSearch._node.classList.remove('search-bar-search-button--selected');
+              return;
+            }
+          }
+
+          searchField.set('value', '');
+          searchForm.submit();
+        }
+      );
+    }
+
+  }
 );
