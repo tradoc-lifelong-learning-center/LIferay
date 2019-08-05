c64cc935003bf6fdd2868defd674acd7f3bee635
Mike Dean <mldean@users.noreply.github.com>
2019-08-05 15:18:14 -0400

    Fixing bug that prevents search from working

--- themes/chroma-tjaglcs-theme/src/js/main.js
+++ themes/chroma-tjaglcs-theme/src/js/main.js
@@ -11,30 +11,28 @@ AUI().ready(
       var searchForm = siteSearch.one('form');
       var searchField = searchForm.one('.search-bar-keywords-input');
 
-
+      //clear search box onload. Does weird things on search page if it has a value
+      searchField.set('value', '');
 
       btnSearch.on(
         'click',
         function(event) {
           event.preventDefault();
 
-
-
           if (searchField.getStyle('width') == '0px') {
             searchField._node.classList.add('search-bar-keywords-input--expanded');
 						btnSearch._node.classList.add('search-bar-search-button--selected');
             searchField.focus();
             return;
-          } else {
-            if (searchField.get('value') == '') {
+          } else if (searchField.get('value') == ''){
               searchField._node.classList.remove('search-bar-keywords-input--expanded');
 							btnSearch._node.classList.remove('search-bar-search-button--selected');
               return;
-            }
+          } else {
+            searchForm.submit();
           }
 
-          searchField.set('value', '');
-          searchForm.submit();
+
         }
       );
     }
