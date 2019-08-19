5f18e91c4bf0f04974add950dbc13a477ad7afb9
Mike Dean <mldean@users.noreply.github.com>
2019-08-19 15:41:19 -0400

    Update form-bot-trap.js

--- js/form-bot-trap/form-bot-trap.js
+++ js/form-bot-trap/form-bot-trap.js
@@ -1,19 +1,53 @@
 (function(){
-  //this is the ID of the <form> element you'd like to protect
-  var form = document.getElementById('_1_WAR_webformportlet_INSTANCE_XW4DVjZBFV8G_fm');
-  //this is the ID if the <input> you're using as a check
-  var field = document.getElementById('_1_WAR_webformportlet_INSTANCE_XW4DVjZBFV8G_field4');
 
-  //field will be hidding with look and field advanced styling:
-  //#_1_WAR_webformportlet_INSTANCE_XW4DVjZBFV8G_fm .control-group:last-of-type {	display:none;}
+  window.onload = setBotTrap;
 
-  form.addEventListener("submit",function(e) {
+  function setBotTrap(){
+    //this is the ID of the <form> element you'd like to protect
+    var formId = "_1_WAR_webformportlet_INSTANCE_tAhXRgkTfBlm_fm";
+    //this is the ID if the <input> you're using as a check
+    var trapFieldId = "_1_WAR_webformportlet_INSTANCE_tAhXRgkTfBlm_field4";
+
+
+    //var formIds = ['_1_WAR_webformportlet_INSTANCE_whYYfcHck9i5_fm', '_1_WAR_webformupdatetjaglcsportlet_INSTANCE_JXx1vdNCNDYY_fm'];
+    //var forms = document.getElementsByTagName("form");
+
+
+    var form = document.getElementById(formId);
+    var field = document.getElementById(trapFieldId);
+    field.parentNode.style.display = "none";
+    var submit = form.getElementsByTagName("button")[0];
+    var description = form.getElementsByClassName("description")[0];
+
+
+    submit.addEventListener('click', function (event) {
+
+      if(field.value!=""){
+        submit.setAttribute("disabled","true")
+
+        //form.innerHTML = "<div class='alert alert-danger'>Form Error!</div>"
+
+        description.insertAdjacentHTML('afterend', "<div class='alert alert-danger'>Form Error!</div>");
+
+        return false;
+      }
+
+
+    }, false);
+  }
+
+
+
+
+  /*form.addEventListener("submit",function(e) {
       e.preventDefault();
+
+      e.target.setAttribute("disabled","true")
 
   	if(field.value!=""){
   		form.setAttribute("action","/web/guest/support-error");
   		return false;
   	}
   	return true
-    });
+  });*/
 })();
